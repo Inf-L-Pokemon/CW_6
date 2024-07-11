2840e643cdc30c31b1e151e90ef9f0f02d87975e
Sergey Korchmin <sergey.korchmin@yandex.ru>
2024-07-11 21:40:12 +0300

    Представление для клиента и сообщения

--- email_list/urls.py
+++ email_list/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 
 from email_list.apps import EmailListConfig
 from email_list.views import ClientListView, ClientDetailView, ClientCreateView, ClientUpdateView, ClientDeleteView, \
-    PlugTemplateView
+    PlugTemplateView, MailingMessageCreateView, MailingMessageListView, MailingMessageDetailView, \
+    MailingMessageUpdateView, MailingMessageDeleteView
 
 app_name = EmailListConfig.name
 
@@ -12,5 +13,10 @@ urlpatterns = [
     path('client/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
     path('client/update/<int:pk>/', ClientUpdateView.as_view(), name='client_update'),
     path('client/delete/<int:pk>/', ClientDeleteView.as_view(), name='client_delete'),
+    path('mailingmessage/create/', MailingMessageCreateView.as_view(), name='mailing_message_create'),
+    path('mailingmessage/list', MailingMessageListView.as_view(), name='mailing_message_list'),
+    path('mailingmessage/<int:pk>/', MailingMessageDetailView.as_view(), name='mailing_message_detail'),
+    path('mailingmessage/update/<int:pk>/', MailingMessageUpdateView.as_view(), name='mailing_message_update'),
+    path('mailingmessage/delete/<int:pk>/', MailingMessageDeleteView.as_view(), name='mailing_message_delete'),
     path('plug/', PlugTemplateView.as_view(), name='plug'),
 ]
--- email_list/views.py
+++ email_list/views.py
@@ -2,8 +2,8 @@ from django.urls import reverse_lazy
 from django.urls.base import reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView, TemplateView
 
-from email_list.forms import ClientForm
-from email_list.models import Client
+from email_list.forms import ClientForm, MailingMessageForm
+from email_list.models import Client, MailingMessage
 
 
 class PlugTemplateView(TemplateView):
@@ -46,3 +46,41 @@ class ClientUpdateView(UpdateView):
 class ClientDeleteView(DeleteView):
     model = Client
     success_url = reverse_lazy('email_list:client_list')
+
+
+class MailingMessageCreateView(CreateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+    success_url = reverse_lazy('email_list:mailing_message_create')
+
+
+class MailingMessageListView(ListView):
+    model = MailingMessage
+
+
+class MailingMessageDetailView(DetailView):
+    model = MailingMessage
+
+
+class MailingMessageUpdateView(UpdateView):
+    model = MailingMessage
+    form_class = MailingMessageForm
+
+    def get_success_url(self):
+        return reverse('email_list:mailing_message_detail', args=[self.kwargs.get('pk')])
+
+    def form_valid(self, form):
+        context_data = self.get_context_data()
+        formset = context_data['formset']
+        if form.is_valid() and formset.is_valid():
+            self.object = form.save()
+            formset.instance = self.object
+            formset.save()
+            return super().form_valid(form)
+        else:
+            return self.render_to_response(self.get_context_data(form=form, formset=formset))
+
+
+class MailingMessageDeleteView(DeleteView):
+    model = MailingMessage
+    success_url = reverse_lazy('email_list:mailing_message_list')
